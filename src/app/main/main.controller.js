f614c43a743639fb4521afea0fe29c922c067094
Katie Graham <katie@deliciousmint.net>
2015-03-04 16:41:04 +0100

    Make changes to main controller and view to have all fields required and go to container view on successful creation of a container

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,23 +1,23 @@
 'use strict';
 
-angular.module('webfactory').controller('MainController', function($scope, $http) {
+angular.module('webfactory').controller('MainController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
   $scope.submitTheForm = function() {
     /* jshint camelcase: false */
     console.log('Submitting form');
     var dataObject = {
-      node_title : 'hello world!',
+      node_title : $scope.nodeTitle,
       hostname : $scope.containerName,
-      site_email : 'katie@innoveto.com',
+      site_email : $scope.siteEmail,
       public : 1,
-      owner_uid : 6
+      owner_uid : 1
     };
 
     var responsePromise = $http.post('http://192.168.1.132:8073/webfact_api/website', dataObject, {});
     responsePromise.success(function(dataFromServer, status, headers, config) {
-      console.log(dataFromServer.hostname);
+      $location.path('/container/' + $scope.containerName);
     });
     responsePromise.error(function(data, status, headers, config) {
       alert('Submitting form failed!');
     });
-  };
-});
+  }
+}]);
